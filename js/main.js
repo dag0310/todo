5eed70ee637f313cbcefbdd5b277089d09bee67d
Daniel Geymayer <dag0310@gmail.com>
2013-05-31 15:56:18 +0200

    Started to change XML format of ToDos to JSON.

--- js/main.js
+++ js/main.js
@@ -4,79 +4,92 @@
  *  date: 2013-05-31
  */
 
-address_dev = "10.63.26.21:98/todo/";
+address_dev = "http://10.0.0.109:98/todo/";
 address_local = "";
 address = address_dev;
 xml_file = "data/todos.xml";
+json_file = "data/todos.json";
 php_add = "data/add_item.php";
 php_del = "data/del_item.php";
 php_delall = "data/del_all.php";
 
-// Add ToDo
-$(document).on('click', '#add_todo', function() {
-	var todo = document.getElementById("todo");
-	if (todo.value.trim() == "") {
-		todo.placeholder = "No empty ToDos allowed!";
+$(function() {
+	$.ajaxSetup({
+		async: false
+	});
+
+	// Add ToDo
+	$(document).on('click', '#add_todo', function() {
+		var todo = document.getElementById("todo");
+		if (todo.value.trim() == "") {
+			todo.placeholder = "No empty ToDos allowed!";
+			refresh_list();
+			return false;
+		}
+		
+		$.ajax({
+			type: "POST",
+			url: address + php_add,
+			cache: false,
+			data: { todo_text: todo.value, file: xml_file },
+			success: refresh_list,
+			error: refresh_list
+		});
+		
+		return false;
+	});
+	
+	// Delete all ToDos
+	$(document).on('click', '#delete_all', function() {
+		$.ajax({
+			type: "POST",
+				url: address + php_delall,
+				cache: false,
+				data: { file: xml_file },
+				success: refresh_list,
+				error: refresh_list
+		});
+		
+		return false;
+	});
+	
+	// Delete the selected ToDo
+	$(document).on('click', 'a', function() {
+		$.ajax({
+			type: "POST",
+			url: address + php_del,
+			cache: false,
+			data: { id: this.id, file: xml_file },
+			success: refresh_list,
+			error: refresh_list
+		});
+		
+		return false;
+	});
+	
+	// Refresh the list
+	$(document).on('click', '#refresh', function() {
 		refresh_list();
 		return false;
-	}
-	
-	$.ajax({
-		type: "POST",
-		url: address + php_add,
-		cache: false,
-		data: { todo_text: todo.value, file: xml_file },
-		success: refresh_list,
-		error: refresh_list
 	});
 	
-	return false;
-});
-
-// Delete all ToDos
-$(document).on('click', '#delete_all', function() {
-$.ajax({
-	type: "POST",
-		url: address + php_delall,
-		cache: false,
-		data: { file: xml_file },
-		success: refresh_list,
-		error: refresh_list
-	});
+	// $.ajax({
+			// dataType: "json",
+			// url: address + json_file,
+			// cache: false,
+			// success: load_items,
+			// error: refresh_list
+		// });
 	
-	return false;
-});
-
-// Refresh the list
-$(document).on('click', '#refresh', function() {
-	refresh_list();
-	return false;
-});
-
-// Delete the selected ToDo
-$(document).on('click', 'a', function() {
-	$.ajax({
-		type: "POST",
-		url: address + php_del,
-		cache: false,
-		data: { id: this.id, file: xml_file },
-		success: refresh_list,
-		error: refresh_list
-	});
-	
-	return false;
-});
-
-$(document).ready(function() {
 	refresh_list();
 });
 
 function refresh_list() {
-	var todos = load_items();
+	var todos = get_todos();
 	var ul = document.getElementById("todos");
 	ul.innerHTML = "";
 	
-	// create list items for all todos in the list and append them to the list
+	// create a list items for all todos in the list and append them to the list
 	for (var i = 0; i < todos.length; i++) {
 		var id = todos[i].id;
 		var text = todos[i].text;
@@ -99,32 +112,31 @@ function refresh_list() {
 	$('ul#todos').listview('refresh');
 }
 
-function load_items() {
-	// load items via AJAX
-	var request = new XMLHttpRequest();
-	request.open("GET", address + xml_file, false);
-	request.send();
-	
+function get_todos() {
 	var todos = new Array();
-	if (request.readyState == 4 && request.status == 200) {
-		// Load items via AJAX
-		var xml_todos = request.responseXML.getElementsByTagName("todo");
-		
-		for (var i = 0; i < xml_todos.length; i++) {
-		    var xml_todo = xml_todos[i];
-		    var todo = new Object();
-			todo.id = parseInt(xml_todo.getAttribute("id"));
-			todo.text = xml_todo.childNodes[0].nodeValue;
-			todos.push(todo);
-		}
-		
-		// Save loaded items into localStorage
-		localStorage.setItem("todos", JSON.stringify(todos));
-	} else {
-		// Load items from localStorage
-		todos = JSON.parse(localStorage.getItem("todos"));
-		$('#notification').text("Showing offline data!");
-	}
+	var color = "red";
 	
-	return (todos != null) ? todos : new Array();
+	$.getJSON(address + json_file, function(data) {
+		$.each(data, function(key, value) {
+			todos.push(new todo(key, value.text));
+		});
+	})
+	.done(function() {
+		console.log("ONLINE");
+		localStorage.setItem("todos", JSON.stringify(todos));
+		color = "green";
+	})
+	.fail(function() {
+		console.log("OFFLINE");
+		todos = JSON.parse(localStorage.getItem("todos"));
+	});
+	
+	// document.body.style.backgroundColor = color;
+	
+	return todos;
+}
+
+function todo(id, text) {
+	this.id = id;
+	this.text = text;
 }
